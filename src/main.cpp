c8318e07fd2af8a56dce2e2df82520d1b5797772
Roberto Groza <robertogroza@gmail.com>
2025-09-03 18:42:17 +0200

    Two encoders working

--- src/main.cpp
+++ src/main.cpp
@@ -3,64 +3,73 @@
 
 class MotorFOC {
 public:
-  MotorFOC(int pwm_a, int pwm_b, int pwm_c, int en, int cs)
-      : motor(7), driver(pwm_a, pwm_b, pwm_c, en), encoder(nullptr), cs(cs), last_print(0) {}
+  MotorFOC(int pwm_a, int pwm_b, int pwm_c, int en, int cs, const char *name)
+      : motor_(7), driver_(pwm_a, pwm_b, pwm_c, en), encoder_(nullptr), cs_(cs), angle_(0), name_(name) {}
 
   void setup() {
-    encoder = new MagneticSensorSPI(AS5048_SPI, cs);
-    encoder->init();
+    encoder_ = new MagneticSensorSPI(AS5048_SPI, cs_);
+    encoder_->init();
 
-    driver.voltage_power_supply = 10;
-    driver.init();
+    driver_.voltage_power_supply = 10;
+    driver_.init();
 
-    motor.linkSensor(encoder);
-    motor.linkDriver(&driver);
+    motor_.linkSensor(encoder_);
+    motor_.linkDriver(&driver_);
 
-    motor.controller = MotionControlType::angle;
+    motor_.controller = MotionControlType::angle;
 
-    motor.PID_velocity.P = 0.2;
-    motor.PID_velocity.I = 20;
-    motor.voltage_limit = 10;
-    motor.LPF_velocity.Tf = 0.02;
-    motor.P_angle.P = 20;
-    motor.velocity_limit = 4;
+    motor_.PID_velocity.P = 0.2;
+    motor_.PID_velocity.I = 20;
+    motor_.voltage_limit = 10;
+    motor_.LPF_velocity.Tf = 0.02;
+    motor_.P_angle.P = 20;
+    motor_.velocity_limit = 4;
 
-    motor.init();
-    motor.initFOC();
+    motor_.init();
+    motor_.initFOC();
 
-    Serial.println("Motor ready.");
+    Serial.printf("%s ready\n", name_);
   }
 
-  void commandMotor(Commander commander, char *cmd) { commander.motion(&motor, cmd); }
+  void commandMotor(Commander commander, char *cmd) { commander.motion(&motor_, cmd); }
 
-  void pollEncoder() {
-    encoder->update();
-    unsigned long now = millis();
-    if (now - last_print >= 500) {
-      last_print = now;
-      Serial.println(encoder->getAngle());
-    }
+  void updateEncoder() {
+    encoder_->update();
+    angle_ = encoder_->getAngle();
   }
+
+  float getAngle() { return angle_; }
+
+  const char *getName() { return name_; }
 
 private:
-  BLDCMotor motor;
-  BLDCDriver3PWM driver;
-  MagneticSensorSPI *encoder;
-  int cs;
-  unsigned long last_print;
+  BLDCMotor motor_;
+  BLDCDriver3PWM driver_;
+  MagneticSensorSPI *encoder_;
+  int cs_;
+  float angle_;
+  const char *name_;
 };
 
-MotorFOC motor1(25, 26, 27, 33, 21);
+MotorFOC motor_left(12, 13, 14, 32, 2, "Left");
+MotorFOC motor_right(25, 26, 27, 33, 4, "Right");
 
 Commander commander = Commander(Serial);
 
-void onTarget(char *cmd) { motor1.commandMotor(commander, cmd); }
+unsigned long last_print = 0;
+
+void onTarget(char *cmd) {
+  motor_left.commandMotor(commander, cmd);
+  motor_right.commandMotor(commander, cmd);
+}
 
 void setup() {
   Serial.begin(115200);
   delay(200);
 
-  motor1.setup();
+  motor_left.setup();
+  motor_right.setup();
+
   commander.add('T', onTarget, "motion control");
 }
 
@@ -69,6 +78,15 @@ void loop() {
   // motor.move();
   // command.run();
 
-  motor1.pollEncoder();
+  motor_left.updateEncoder();
+  motor_right.updateEncoder();
+
+  unsigned long now = millis();
+  if (now - last_print >= 500) {
+    last_print = now;
+    Serial.printf("%s: %.2f\t%s: %.2f\n", motor_left.getName(), motor_left.getAngle(), motor_right.getName(),
+                  motor_right.getAngle());
+  }
+
   delay(50);
 }
